e98eb2c181869f59b05f479a6c53be98bc07b7a9
tjouliab <tim.jouliab@icloud.com>
2025-05-09 19:13:56 +0200

    update set objects benchmark

--- src/set-objects/benchmark.ts
+++ src/set-objects/benchmark.ts
@@ -2,6 +2,7 @@ import * as Benchmark from "benchmark";
 import { SetObjectsHash } from "./set-objects-hash";
 import { MockObjectSimple } from "../mock.utils";
 import { BenchmarkUtils } from "../benchmark.utils";
+import { SetObjectsComp } from "./set-objects-comp";
 
 /**
  * This file contains a benchmark suite to compare the performance of two functions:
@@ -22,9 +23,31 @@ function filterListWithUniqueId(
   }
   return result;
 }
+function filterListWithUniqueIdSorted(
+  objects: MockObjectSimple[]
+): MockObjectSimple[] {
+  const result: MockObjectSimple[] = [];
+  for (const object of objects) {
+    if (!result.some((elem) => elem.id === object.id)) {
+      result.push(object);
+    }
+  }
+  return result.sort((a, b) => a.id - b.id);
+}
 
-function createSetObjects(objects: MockObjectSimple[]): MockObjectSimple[] {
+function createSetObjectsHash(objects: MockObjectSimple[]): MockObjectSimple[] {
   return [...new SetObjectsHash(objects, (object) => object.id)];
+}
+function createSetObjectsHashSorted(
+  objects: MockObjectSimple[]
+): MockObjectSimple[] {
+  return [...new SetObjectsHash(objects, (object) => object.id)].sort(
+    (a, b) => a.id - b.id
+  );
+}
+
+function creatSetObjectsComp(objects: MockObjectSimple[]): MockObjectSimple[] {
+  return new SetObjectsComp(objects, (object) => object.id).toList();
 }
 
 // Create a new benchmark suite
@@ -33,7 +56,13 @@ const suite = new Benchmark.Suite();
 // Add tests to the suite
 BenchmarkUtils.addObjectSimple(
   suite,
-  [filterListWithUniqueId, createSetObjects],
+  [
+    filterListWithUniqueId,
+    filterListWithUniqueIdSorted,
+    createSetObjectsHash,
+    createSetObjectsHashSorted,
+    creatSetObjectsComp,
+  ],
   [10, 100, 1_000, 10_000, 100_000, 1_000_000]
 );
 
